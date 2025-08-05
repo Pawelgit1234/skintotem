ea67f5deea2f5d2387ecb68806e881166cd40eab
Pawelgit1234 <paul1walter4@gmail.com>
2025-08-05 11:42:42 +0200

    init

---
+++ src/main/java/spichka/skintotem/SkinTotem.java
@@ -0,0 +1,15 @@
+package spichka.skintotem;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SkinTotem implements ModInitializer {
+	public static final String MOD_ID = "skintotem";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/spichka/skintotem/SkinTotemDataGenerator.java
@@ -0,0 +1,11 @@
+package spichka.skintotem;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SkinTotemDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
